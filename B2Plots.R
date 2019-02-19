ba717e46953341f4422f60cd63ee5f59eb214ce9
pcmurphy <murphyp@email.arizona.edu>
2019-02-19 14:16:59 -0700

    Initial commit

---
+++ B2Plots.R
@@ -0,0 +1,404 @@
+####
+#
+# Data plotting script for GBG Biosphere 2 AgriVoltaics site
+# Author: P. Murphy
+# Email: murphyp@email.arizona.edu
+# Date: 2019-01-21
+# Revised: 2019-02-19
+#
+###
+
+
+###
+#
+# This script is designed to generate rough plots of data from the B2 AgriVoltaics site to 
+# look for sensor malfunction, extreme events, and overall site status.
+#
+# Plots generated:
+#   -VWC (Volumetric Water Content) of both the control and experiment (PV) gardens
+#   -Soil Temperature of both the control and experiment (PV) gardens
+#   -Micromet (Air Temperature and RH) of both the control and experiment (PV) gardens
+#   -Panel temperature of both the control (no garden) and experiment (garden) panels
+#
+# Successful running of this script requires the following two variables be set:
+collectionDate <- "2019_01_30"  # format collectionDate as "yyyy_mm_dd", otherwise the script won't work
+plotDays <- 16  # define how many days prior to the collection date that you want to plot
+#
+# Save the script, and then source it:
+#   -Click Source in RStudio
+# OR
+#   -In the console, run: source("B2Plots.R")
+#
+# Errors will cause the script to fail, Warning messages indicate something wasn't quite right,
+# but the script continued anyway.
+#
+###
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####
+# Variables
+#
+
+# Static
+if(.Platform$OS.type == "unix") {
+  dataPath <- "/Volumes/shares/SGD_BIO2/Biosphere 2/B2 Solar Yard & Agrivoltaics/Agrivoltaics Campbell Datalogger/data/manual downloads/"
+} else {
+  dataPath <- "//boom.sbs.arizona.edu/shares/SGD_BIO2/Biosphere 2/B2 Solar Yard & Agrivoltaics/Agrivoltaics Campbell Datalogger/data/manual downloads/"
+}
+
+recentDate <- as.POSIXct(paste(substring(collectionDate,1,4),substring(collectionDate,6,7),substring(collectionDate,9,10), sep = "-"))
+firstDate <- recentDate-(86400*plotDays)
+fileDate <- paste0(substring(collectionDate,1,4),substring(collectionDate,6,7),substring(collectionDate,9,10))
+dataFileC <- paste0(dataPath,collectionDate,"/B2_Ag_C_CR1000_Table1_",fileDate,".dat")
+dataFilePV <- paste0(dataPath,collectionDate,"/B2_Ag_PV_CR1000_Table1_",fileDate,".dat")
+dataFilePVcontrol <- paste0(dataPath,collectionDate,"/B2_Ag_PVcontrol_CR1000_Table1_",fileDate,".dat")
+
+
+##### 
+# Functions
+#
+#source("/Users/pmurphy/Box Sync/R/toolbox/clear.R")
+
+
+
+#####
+# File control
+dir.create(paste0("/Users/pmurphy/Box Sync/GBG/figures/B2Agrivoltaics/",collectionDate))
+setwd(paste0("/Users/pmurphy/Box Sync/GBG/figures/B2Agrivoltaics/",collectionDate))
+
+
+#####
+# Import data
+B2ColumnsC <- read.csv(file = dataFileC, skip = 1)
+B2DataC <- read.csv(file = dataFileC, skip = 3)
+colnames(B2DataC) <- colnames(B2ColumnsC)
+rm(B2ColumnsC)
+
+B2ColumnsPV <- read.csv(file = dataFilePV, skip = 1)
+B2DataPV <- read.csv(file = dataFilePV, skip = 3)
+colnames(B2DataPV) <- colnames(B2ColumnsPV)
+rm(B2ColumnsPV)
+
+B2ColumnsPVcontrol <- read.csv(file = dataFilePVcontrol, skip = 1)
+B2DataPVcontrol <- read.csv(file = dataFilePVcontrol, skip = 3)
+colnames(B2DataPVcontrol) <- colnames(B2ColumnsPVcontrol)
+rm(B2ColumnsPVcontrol)
+
+
+#####
+# Process data
+#
+
+# Adjust time to POSIX format
+B2DataC$TIMESTAMP <- as.POSIXct(B2DataC$TIMESTAMP, format="%Y-%m-%d %H:%M")
+B2DataPV$TIMESTAMP <- as.POSIXct(B2DataPV$TIMESTAMP, format="%Y-%m-%d %H:%M")
+B2DataPVcontrol$TIMESTAMP <- as.POSIXct(B2DataPVcontrol$TIMESTAMP, format="%Y-%m-%d %H:%M")
+
+# Generate dataframes within specified dates specifically for plotting
+B2DataPlotC <- B2DataC[B2DataC$TIMESTAMP<recentDate & B2DataC$TIMESTAMP>firstDate,]
+B2DataPlotPV <- B2DataPV[B2DataPV$TIMESTAMP<recentDate & B2DataPV$TIMESTAMP>firstDate,]
+B2DataPlotPVcontrol <- B2DataPVcontrol[B2DataPVcontrol$TIMESTAMP<recentDate & B2DataPVcontrol$TIMESTAMP>firstDate,]
+
+# Ensure columns are numeric format (often not the case if all NaN, will break plotting)
+B2DataPlotC[,2:ncol(B2DataPlotC)] <- sapply(B2DataPlotC[,2:ncol(B2DataPlotC)],as.numeric)
+B2DataPlotPV[,2:ncol(B2DataPlotPV)] <- sapply(B2DataPlotPV[,2:ncol(B2DataPlotPV)],as.numeric)
+B2DataPlotPVcontrol[,2:ncol(B2DataPlotPVcontrol)] <- sapply(B2DataPlotPVcontrol[,2:ncol(B2DataPlotPVcontrol)],as.numeric)
+
+
+
+#####
+# Plot most recent data
+#
+
+# VWC
+colsVWC <- c("Black",rgb(103,0,31, max = 255),rgb(214,96,77, max = 255),rgb(244,165,130, max = 255),rgb(253,219,199, max = 255),rgb(247,247,247, max = 255),rgb(209,229,240, max = 255),rgb(146,197,222, max = 255),rgb(67,147,195, max = 255),rgb(33,102,172, max = 255),rgb(5,48,97, max = 255),rgb(103,0,31, max = 255))
+
+# PV garden
+png(file = paste0("VWC_PVgarden_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd=1)
+plot(x = B2DataPlotPV$TIMESTAMP,
+     y = B2DataPlotPV$VWC.1.,
+     col = colsVWC[1],
+     type = "l",
+     ylim = c(0,0.6),
+     ylab = expression("VWC (m"^{3} ~ "m"^{-3}* ")"),
+     xlab = "",
+     xaxt = "n"
+     )
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+for (i in c(2:12)) {
+  plotNum <- paste0("VWC.",i,".")
+  
+  lines(x = B2DataPlotPV$TIMESTAMP,
+        y = B2DataPlotPV[,plotNum],
+        col = colsVWC[i])
+}
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("bottomleft",
+       legend=paste0("PV",seq(1:12)),
+       col=colsVWC,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+# Control garden
+png(file = paste0("VWC_Cgarden_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd=1)
+plot(x = B2DataPlotC$TIMESTAMP,
+     y = B2DataPlotC$VWC.1.,
+     col = colsVWC[1],
+     type = "l",
+     ylim = c(0,0.6),
+     ylab = expression("VWC (m"^{3} ~ "m"^{-3}* ")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+for (i in c(2:12)) {
+  plotNum <- paste0("VWC.",i,".")
+  
+  lines(x = B2DataPlotC$TIMESTAMP,
+        y = B2DataPlotC[,plotNum],
+        col = colsVWC[i])
+}
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("bottomleft",
+       legend=paste0("C",seq(1:12)),
+       col=colsVWC,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+# soilT
+colsSoilT <- c("Black",rgb(103,0,31, max = 255),rgb(214,96,77, max = 255),rgb(244,165,130, max = 255),rgb(253,219,199, max = 255),rgb(247,247,247, max = 255),rgb(209,229,240, max = 255),rgb(146,197,222, max = 255),rgb(67,147,195, max = 255),rgb(33,102,172, max = 255),rgb(5,48,97, max = 255),rgb(103,0,31, max = 255))
+# PV garden
+png(file = paste0("SoilT_PVgarden_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd=1)
+plot(x = B2DataPlotPV$TIMESTAMP,
+     y = B2DataPlotPV$Temp.1.,
+     col = colsSoilT[1],
+     type = "l",
+     ylim = c(0,50),
+     ylab = expression("Soil Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+for (i in c(2:12)) {
+  plotNum <- paste0("Temp.",i,".")
+  
+  lines(x = B2DataPlotPV$TIMESTAMP,
+        y = B2DataPlotPV[,plotNum],
+        col = colsSoilT[i])
+}
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("bottomleft",
+       legend=paste0("PV",seq(1:12)),
+       col=colsSoilT,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+# Control garden
+png(file = paste0("SoilT_Cgarden_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd=1)
+plot(x = B2DataPlotC$TIMESTAMP,
+     y = B2DataPlotC$Temp.1.,
+     col = colsSoilT[1],
+     type = "l",
+     ylim = c(0,50),
+     ylab = expression("Soil Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+for (i in c(2:12)) {
+  plotNum <- paste0("Temp.",i,".")
+  
+  lines(x = B2DataPlotC$TIMESTAMP,
+        y = B2DataPlotC[,plotNum],
+        col = colsSoilT[i])
+}
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("bottomleft",
+       legend=paste0("C",seq(1:12)),
+       col=colsSoilT,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+# Micromet
+colsMet <- c("blue","red","cadetblue2","orange")
+png(file = paste0("met_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(4,5,1,4), lwd=1)
+plot(x = B2DataPlotPV$TIMESTAMP,
+     y = B2DataPlotPV$AirTC_PV1_Avg,
+     col = colsMet[1],
+     type = "l",
+     ylim = c(-10,50),
+     ylab = expression("Air Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = B2DataPlotC$TIMESTAMP,
+      y = B2DataPlotC$AirTC_C_Avg,
+      col = colsMet[2])
+par(new = T)
+plot(x = B2DataPlotPV$TIMESTAMP,
+     y = B2DataPlotPV$RH_PV1,
+     col = colsMet[3],
+     type = "l",
+     axes = F,
+     xlab = NA,
+     ylab = NA
+     )
+axis(side = 4)
+mtext(side = 4, line = 3, "Relative Humidity (%)")
+lines(x = B2DataPlotC$TIMESTAMP,
+      y = B2DataPlotC$RH_C,
+      col = colsMet[4])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("AirT PV","AirT C","RH PV","RH C"),
+       col=colsMet,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+# PAR
+colsPAR <- c("blue","cadetblue2","red")
+png(file = paste0("PAR_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(4,5,1,1), lwd=1)
+plot(x = B2DataPlotPV$TIMESTAMP,
+     y = B2DataPlotPV$PAR_Den_PV1_Avg,
+     col = colsPAR[1],
+     type = "l",
+     ylim = c(min(B2DataPlotC[,"PAR_Den_C_Avg"], na.rm = T),max(B2DataPlotC[,c("PAR_Den_C_Avg")], na.rm = T)),
+     ylab = expression("PAR Density (" * mu * "mol"~ m^{-2} ~ s^{-1} * ")"),
+     xlab = "Date"#,
+     #xaxt = "n"
+)
+#axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = B2DataPlotPV$TIMESTAMP,
+      y = B2DataPlotPV$PAR_Den_PV2_Avg,
+      col = colsPAR[2])
+lines(x = B2DataPlotC$TIMESTAMP,
+      y = B2DataPlotC$PAR_Den_C_Avg,
+      col = colsPAR[3])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("PV1","PV2","C"),
+       col=colsPAR,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+
+# Solar panel temp
+colsSolarT <- rep(c("Black",rgb(103,0,31, max = 255),rgb(214,96,77, max = 255),rgb(244,165,130, max = 255),rgb(253,219,199, max = 255),rgb(247,247,247, max = 255),rgb(209,229,240, max = 255),rgb(146,197,222, max = 255),rgb(67,147,195, max = 255),rgb(33,102,172, max = 255),rgb(5,48,97, max = 255),rgb(103,0,31, max = 255)),2)
+
+# PV garden
+png(file = paste0("solarT_PVgarden_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd=1)
+plot(x = B2DataPlotPV$TIMESTAMP,
+     y = B2DataPlotPV$SEVolt_Avg.1.,
+     col = colsSolarT[1],
+     type = "l",
+     ylim = c(0,200),
+     ylab = expression("PV Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+for (i in c(2:24)) {
+  plotNum <- paste0("SEVolt_Avg.",i,".")
+  
+  lines(x = B2DataPlotPV$TIMESTAMP,
+        y = B2DataPlotPV[,plotNum],
+        col = colsSolarT[i])
+}
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("bottomleft",
+       legend=paste0("Garden",seq(1:24)),
+       col=colsSolarT,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+# Control garden
+png(file = paste0("solarT_controlGarden_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd=1)
+plot(x = B2DataPlotPVcontrol$TIMESTAMP,
+     y = B2DataPlotPVcontrol$SEVolt_Avg.1.,
+     col = colsSolarT[1],
+     type = "l",
+     ylim = c(0,200),
+     ylab = expression("PV Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+for (i in c(2:24)) {
+  plotNum <- paste0("SEVolt_Avg.",i,".")
+  
+  lines(x = B2DataPlotPVcontrol$TIMESTAMP,
+        y = B2DataPlotPVcontrol[,plotNum],
+        col = colsSolarT[i])
+}
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+# legend("bottomleft",
+#        legend=paste0("Garden",seq(1:24)),
+#        col=colsSolarT,
+#        lty=1,
+#        cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ manzoPlots.R
@@ -0,0 +1,376 @@
+####
+#
+# Data plotting script for GBG Manzo Elementary AgriVoltaics site
+# Author: P. Murphy
+# Email: murphyp@email.arizona.edu
+# Date: 2019-01-16
+# Revised: 2019-02-06
+#
+###
+
+
+###
+#
+# This script is designed to generate rough plots of data from the Manzo AgriVoltaics site to 
+# look for sensor malfunction, extreme events, and overall site status.
+#
+# Plots generated:
+#   -VWC (Volumetric Water Content) of both the control and experiment (PV) gardens
+#   -Soil Temperature of both the control and experiment (PV) gardens
+#   -Micromet (Air Temperature and RH) of both the control and experiment (PV) gardens
+#   -Panel temperature experiment (garden) panels
+#
+# Successful running of this script requires the following two variables be set:
+collectionDate <- "2019_02_13"  # format collectionDate as "yyyy_mm_dd", otherwise the script won't work
+plotDays <- 26  # define how many days prior to the collection date that you want to plot
+#
+# Save the script, and then source it:
+#   -Click Source in RStudio
+# OR
+#   -In the console, run: source("manzoPlots.R")
+#
+# Errors will cause the script to fail, Warning messages indicate something wasn't quite right,
+# but the script continued anyway.
+#
+###
+
+
+
+
+
+
+
+
+
+
+#####
+# Variables
+
+# Static
+if(.Platform$OS.type == "unix") {
+  dataPath <- "/Volumes/shares/SGD_BIO2/Biosphere 2/B2 Solar Yard & Agrivoltaics/Agrivoltaics Campbell Datalogger/data/MANZO/"
+} else {
+  dataPath <- "//boom.sbs.arizona.edu/shares/SGD_BIO2/Biosphere 2/B2 Solar Yard & Agrivoltaics/Agrivoltaics Campbell Datalogger/data/MANZO/"
+}
+
+recentDate <- as.POSIXct(paste(substring(collectionDate,1,4),substring(collectionDate,6,7),substring(collectionDate,9,10), sep = "-"))
+firstDate <- recentDate-(86400*plotDays)
+fileDate <- paste0(substring(collectionDate,1,4),substring(collectionDate,6,7),substring(collectionDate,9,10))
+dataFile <- paste0(dataPath,collectionDate,"/ManzoCR1000_Table1_",fileDate,".dat")
+
+
+##### 
+# Functions
+#source("/Users/pmurphy/Box Sync/R/toolbox/clear.R")
+
+#####
+# File control
+dir.create(paste0("/Users/pmurphy/Box Sync/GBG/figures/manzoAgrivoltaics/",collectionDate))
+setwd(paste0("/Users/pmurphy/Box Sync/GBG/figures/manzoAgrivoltaics/",collectionDate))
+
+
+#####
+# Import data
+manzoColumns <- read.csv(file = dataFile, skip = 1)
+manzoData <- read.csv(file = dataFile, skip = 3)
+colnames(manzoData) <- colnames(manzoColumns)
+rm(manzoColumns)
+
+
+#####
+# Process data
+
+# Adjust time to POSIX format
+manzoData$TIMESTAMP <- as.POSIXct(manzoData$TIMESTAMP, format="%Y-%m-%d %H:%M")
+
+# Generate dataframe within specified dates specifically for plotting
+manzoDataPlot <- manzoData[manzoData$TIMESTAMP<recentDate & manzoData$TIMESTAMP>firstDate,]
+
+# Ensure columns are numeric format (often not the case if all NaN, will break plotting)
+manzoDataPlot[,2:ncol(manzoDataPlot)] <- sapply(manzoDataPlot[,2:ncol(manzoDataPlot)],as.numeric)
+
+
+
+#####
+# Plot most recent data
+
+# VWC
+colsVWC <- c("violet","blue","cadetblue2","red","orange","yellow")
+png(file = paste0("VWC_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd = 1)
+plot(x = manzoDataPlot$TIMESTAMP,
+     y = manzoDataPlot$PV_VWC1,
+     col = colsVWC[1],
+     type = "l",
+     ylim = c(min(manzoDataPlot[,c("PV_VWC1","PV_VWC2","PV_VWC3","C_VWC1","C_VWC2","C_VWC3")], na.rm = T),max(manzoDataPlot[,c("PV_VWC1","PV_VWC2","PV_VWC3","C_VWC1","C_VWC2","C_VWC3")], na.rm = T)),
+     #ylab = expression("VWC (m"^{3} ~ "m"^{-3}* ")"),
+     ylab = "VWC (%)",
+     xlab = "",
+     xaxt = "n"
+     )
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_VWC2,
+      col = colsVWC[2])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_VWC3,
+      col = colsVWC[3])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$C_VWC1,
+      col = colsVWC[4])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$C_VWC2,
+      col = colsVWC[5])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$C_VWC3,
+      col = colsVWC[6])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("PV1", "PV2", "PV3", "C1", "C2", "C3"),
+       col=colsVWC,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+# soilT
+colsSoilT <- c("violet","blue","cadetblue2","red","orange","yellow")
+png(file = paste0("soilT_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd = 1)
+plot(x = manzoDataPlot$TIMESTAMP,
+     y = manzoDataPlot$PV_Temp1,
+     col = colsSoilT[1],
+     type = "l",
+     ylim = c(min(manzoDataPlot[,c("C_Temp1","C_Temp2","C_Temp3")]),max(manzoDataPlot[,c("C_Temp1","C_Temp2","C_Temp3")])),
+     ylab = expression("Soil Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Temp2,
+      col = colsSoilT[2])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Temp3,
+      col = colsSoilT[3])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$C_Temp1,
+      col = colsSoilT[4])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$C_Temp2,
+      col = colsSoilT[5])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$C_Temp3,
+      col = colsSoilT[6])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("PV1", "PV2", "PV3", "C1", "C2", "C3"),
+       col=colsSoilT,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+# Micromet
+colsMet <- c("blue","red","cadetblue2","orange")
+png(file = paste0("met_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,4), lwd = 1)
+plot(x = manzoDataPlot$TIMESTAMP,
+     y = manzoDataPlot$AirTC_PV_Avg,
+     col = colsMet[1],
+     type = "l",
+     ylim = c(min(manzoDataPlot[,c("AirTC_PV_Avg","AirTC_C_Avg")]),max(manzoDataPlot[,c("AirTC_PV_Avg","AirTC_C_Avg")])),
+     ylab = expression("Air Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$AirTC_C_Avg,
+      col = colsMet[2])
+par(new = T)
+plot(x = manzoDataPlot$TIMESTAMP,
+     y = manzoDataPlot$RH_PV,
+     col = colsMet[3],
+     type = "l",
+     axes = F,
+     xlab = NA,
+     ylab = NA
+     )
+axis(side = 4)
+mtext(side = 4, line = 3, "Relative Humidity (%)")
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$RH_C,
+      col = colsMet[4])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("AirT PV","AirT C","RH PV","RH C"),
+       col=colsMet,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+# PAR
+colsPAR <- c("blue","red")
+png(file = paste0("PAR_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd = 1)
+plot(x = manzoDataPlot$TIMESTAMP,
+     y = manzoDataPlot$PAR_Den_PV_Avg,
+     col = colsPAR[1],
+     type = "l",
+     ylim = c(min(manzoDataPlot[,c("PAR_Den_PV_Avg","PAR_Den_C_Avg")]),max(manzoDataPlot[,c("PAR_Den_PV_Avg","PAR_Den_C_Avg")])),
+     ylab = expression("PAR Density (" * mu * "mol"~ m^{-2} ~ s^{-1} * ")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PAR_Den_C_Avg,
+      col = colsPAR[2])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("PV","C"),
+       col=colsPAR,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+# Solar Temp
+colsSolarT <- c("violet","blue","cadetblue2","red","orange","yellow")
+# Garden
+png(file = paste0("SolarTgarden_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd = 1)
+plot(x = manzoDataPlot$TIMESTAMP,
+     y = manzoDataPlot$PV_Garden_1_Avg,
+     col = colsSolarT[1],
+     type = "l",
+     ylim = c(0,120),
+     ylab = expression("PV Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Garden_2_Avg,
+      col = colsVWC[2])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Garden_3_Avg,
+      col = colsVWC[3])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Garden_4_Avg,
+      col = colsVWC[4])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Garden_5_Avg,
+      col = colsVWC[5])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Garden_6_Avg,
+      col = colsVWC[6])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("Garden1", "Garden2", "Garden3", "Garden4", "Garden5", "Garden6"),
+       col=colsVWC,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+# Classroom
+png(file = paste0("SolarTclassroom_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd = 1)
+plot(x = manzoDataPlot$TIMESTAMP,
+     y = manzoDataPlot$PV_Classroom_1_Avg,
+     col = colsSolarT[1],
+     type = "l",
+     ylim = c(0,120),
+     ylab = expression("PV Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Classroom_2_Avg,
+      col = colsVWC[2])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Classroom_3_Avg,
+      col = colsVWC[3])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Classroom_4_Avg,
+      col = colsVWC[4])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Classroom_5_Avg,
+      col = colsVWC[5])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Classroom_6_Avg,
+      col = colsVWC[6])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("Classroom1", "Classroom2", "Classroom3", "Classroom4", "Classroom5", "Classroom6"),
+       col=colsVWC,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+# Turf
+png(file = paste0("SolarTturf_",collectionDate,".png"), width = 2500, height = 1000, res = 300)
+par(mar=c(2,5,1,1), lwd = 1)
+plot(x = manzoDataPlot$TIMESTAMP,
+     y = manzoDataPlot$PV_Turf_1_Avg,
+     col = colsSolarT[1],
+     type = "l",
+     ylim = c(0,120),
+     ylab = expression("PV Temp ("*degree*C*")"),
+     xlab = "",
+     xaxt = "n"
+)
+axis.POSIXct(1, at = seq(firstDate, recentDate, by = "5 days"), format = "%Y-%m-%d")
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Turf_2_Avg,
+      col = colsVWC[2])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Turf_3_Avg,
+      col = colsVWC[3])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Turf_4_Avg,
+      col = colsVWC[4])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Turf_5_Avg,
+      col = colsVWC[5])
+lines(x = manzoDataPlot$TIMESTAMP,
+      y = manzoDataPlot$PV_Turf_6_Avg,
+      col = colsVWC[6])
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+     lwd = par("lwd"), equilogs = TRUE)
+legend("topleft",
+       legend=c("Turf1", "Turf2", "Turf3", "Turf4", "Turf5", "Turf6"),
+       col=colsVWC,
+       lty=1,
+       cex=0.8)
+box(lty = "solid", col = "black")
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
